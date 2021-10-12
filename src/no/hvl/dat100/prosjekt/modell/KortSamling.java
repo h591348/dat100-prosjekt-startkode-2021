818f080330b1334c8126b20305eb67ad8f501c3d
598998 <598998@stud.hvl.no>
2021-10-12 14:12:43 +0200

    fjern - FERDIG!!

--- src/no/hvl/dat100/prosjekt/modell/KortSamling.java
+++ src/no/hvl/dat100/prosjekt/modell/KortSamling.java
@@ -176,7 +176,7 @@ public class KortSamling {
 			 
 	public boolean fjern(Kort kort) {
 		for (int k = 0; k < antall; k++){
-			if (samling[k].equals(kort) && antall!=0) {
+			if (samling[k].equals(kort)) {
 				samling[k] = samling[antall-1];
 					samling[antall-1] = null;
 				antall --;
