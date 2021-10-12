031559171be310c72723401a23cc9c9be559a586
h600878 <600878@stud.hvl.no>
2021-10-12 20:12:46 +0200

    sorter() mer leslig kode

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -2,8 +2,6 @@ package no.hvl.dat100.prosjekt.modell;
 
 import java.util.Random;
 
-import no.hvl.dat100.prosjekt.TODO;
-
 public class KortUtils {
 
 	/**
@@ -17,26 +15,27 @@ public class KortUtils {
 	
 	public static void sorter(KortSamling samling) { //FERDIG
 
-		Kort[] nySamling = new Kort[samling.getAntalKort()];
+		Kort[] nyTab = new Kort[samling.getAntalKort() ];
+		Kort[] tab = samling.getSamling();
 
 		//Leter igjennom alle Kort i tabellen
-		for (int tallnr = 0; tallnr < nySamling.length; tallnr++) {
+		for (int tallnr = 0; tallnr < nyTab.length; tallnr++) {
 
 			int minst = 0; //Minste posisjon
 			//Finner minste kort i tabellen...
 			for (int pos = 1; pos < samling.getAntalKort() - tallnr; pos++) { //Antall kort i "samling" synker med 1 for hvert kort som fjernes
 
 				//Returnerer objekt A minus objekt B. Hvis svaret er mindre enn 0, setter vi minst til posisjon A.
-				if (samling.getSamling()[pos].compareTo(samling.getSamling()[minst]) < 0) {
+				if (tab[pos].compareTo(tab[minst]) < 0) {
 
 					minst = pos; //Setter posisjonen til A som ny minst.
 				}
 			}
-			nySamling[tallnr] = samling.getSamling()[minst]; //Setter det minste objektet til første ledige posisjon i nySamling
-			samling.fjern(samling.getSamling()[minst]); //Fjerner kortet minst etter å ha satt de inn i ny tabell, og senker antall med 1
+			nyTab[tallnr] = tab[minst]; //Setter det minste objektet til første ledige posisjon i nySamling
+			samling.fjern(tab[minst]); //Fjerner kortet minst etter å ha satt de inn i ny tabell, og senker antall med 1
 		}
 		//Kopierer ny tabell tilbake til samling tabell, øker antall med 1 for hvert kort som settes inn.
-		for (Kort kort : nySamling) {
+		for (Kort kort : nyTab) {
 			samling.leggTil(kort);
 		}
 	}
