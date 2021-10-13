e51016897c48f7055fa873feee43eae4f457687d
h591348 <eskram2001@gmail.com>
2021-10-13 13:50:54 +0200

    Update Bord.java

--- src/no/hvl/dat100/prosjekt/kontroll/Bord.java
+++ src/no/hvl/dat100/prosjekt/kontroll/Bord.java
@@ -53,7 +53,7 @@ public class Bord {
 	 * 
 	 * @return true om til-bunken er tom, false ellers.
 	 */
-	public boolean bunketilTom() {  //FERDIG   Bruker metoden erTom fra KortSamling.java
+	public boolean bunketilTom() {  //FERDIG | Sjekker om TilBunken er tom | Bruker metoden erTom fra KortSamling.java
 
 		return bunkeTil.erTom();
 	}
@@ -63,7 +63,7 @@ public class Bord {
 	 * 
 	 * @return true om fra-bunken er tom, false ellers.
 	 */
-	public boolean bunkefraTom() {  //FERDIG  Bruker metoden erTom fra KortSamling.java
+	public boolean bunkefraTom() {  //FERDIG | Sjekker om FraBunken er tom | Bruker metoden erTom fra KortSamling.java
 
 		return bunkeFra.erTom();
 
@@ -74,7 +74,7 @@ public class Bord {
 	 * 
 	 * @return antall kort i fra-bunken.
 	 */
-	public int antallBunkeFra() {  //FERDIG  Bruker metoden getAntalKort fra KortSamling.java
+	public int antallBunkeFra() {  //FERDIG | Henter antall kort i FraBunken | Bruker metoden getAntalKort fra KortSamling.java
 		
 		return bunkeFra.getAntalKort();
 	}
@@ -84,7 +84,7 @@ public class Bord {
 	 * 
 	 * @return antall kort i til-bunken.
 	 */
-	public int antallBunkeTil() {  //FERDIG  Bruker metoden getAntalKort fra KortSamling.java
+	public int antallBunkeTil() {  //FERDIG | Henter antall kort i TilBunken | Bruker metoden getAntalKort fra KortSamling.java
 		
 		return bunkeTil.getAntalKort();
 	}
@@ -106,7 +106,7 @@ public class Bord {
 	 * @return peker/referanse til det kort som blev tatt fra fra-bunken
 	 */
 	
-	public Kort taOversteFraBunke() {  //FERDIG  Bruker metoden taSiste fra KortSamling.java
+	public Kort taOversteFraBunke() {  //FERDIG | Tar øverste kort fra FraBunken | Bruker metoden taSiste fra KortSamling.java
 
 		return bunkeFra.taSiste();
 	}
@@ -117,7 +117,7 @@ public class Bord {
 	 * 
 	 * @return peker/referanse til øverste kortet i til-bunken.
 	 */
-	public Kort seOversteBunkeTil() {  //FERDIG  Bruker metoden seSiste fra KortSamling.java
+	public Kort seOversteBunkeTil() {  //FERDIG | Ser på øverste kort i TilBunken | Bruker metoden seSiste fra KortSamling.java
 		
 		return bunkeTil.seSiste();
 	}
@@ -153,7 +153,7 @@ public class Bord {
 	 * 			kort som skal legges ned. 
 	 * 	
 	 */
-	public void leggNedBunkeTil(Kort k) {  //FERDIG  Bruker metoden leggTil fra KortSamling.java
+	public void leggNedBunkeTil(Kort k) {  //FERDIG | Legger ned et kort på TilBunken | Bruker metoden leggTil fra KortSamling.java.
 		
 		getBunkeTil().leggTil(k);
 				
